4daadcdad95e8b19a07db13b7f280e45b2ddecb8
amir-shawkat <amirshawkat05@gmail.com>
2026-02-21 00:26:38 +0600

    Some finishing task to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,88 @@
 import React from "react";
 import Search from "./components/Search";
-import { useState } from "react";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+import { useState , useEffect } from "react";
+import { useDebounce } from "react-use";
+import { updateSearchCount , getTrendingMovies } from "./appwrite.js";
+
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // Ensure this is set in your .env file
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`
+  },
+} 
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+
+  useDebounce(() => {
+    setDebouncedSearchTerm(searchTerm);
+  }, 500, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endPoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+      const response = await fetch(endPoint, API_OPTIONS);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies.");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+      setErrorMessage("");
+      
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const trending = await getTrendingMovies();
+      setTrendingMovies(trending);
+    } catch (error) {
+      console.error("Error loading trending movies:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
@@ -13,9 +91,40 @@ const App = () => {
         <header>
           <img src="/hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient"> Movies </span> You'll Enjoy Without the Hassle on <span className="text-gradient">CineHub</span></h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />  
         </header>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie , index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} loading="lazy" />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                 
+                <MovieCard movie={movie} />
+
+              ))}
+            </ul>
+          )}
+        </section>
+
         <h1>{searchTerm ? `Search results for "${searchTerm}"` : null}</h1>
       </div>
     </main>
@@ -25,4 +134,4 @@ const App = () => {
 export default App;
 
 
-/* 1hr 07min */
+/* 1hr 27min */
